f1ed1b5ad3244c47299a5b3efb85c6d3d2406672
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-02-16 13:24:30 +0600

    reversing an array with/without reverse method

---
+++ Practice/JavaScript/reverse-array.js
@@ -0,0 +1,27 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+// numbers.reverse();               //method-1
+// console.log(numbers);
+
+// let rev_numbers = [];            //method-2
+// for(let num of numbers) {
+//     rev_numbers.unshift(num);
+// }
+// console.log(rev_numbers);
+
+// let rev_numbers = [];            //method-3
+// let num;
+// for (let i = 0; i < numbers.length; i++) {
+//     // // num = numbers[i];
+//     // // rev_numbers.unshift(num);
+//     rev_numbers.unshift(numbers[i]);
+// }
+// console.log(rev_numbers);
+
+// let rev_numbers = [];            //method-4
+// let num;
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//     num = numbers[i];
+//     rev_numbers.push(num);
+// }
+
+// console.log(rev_numbers);
