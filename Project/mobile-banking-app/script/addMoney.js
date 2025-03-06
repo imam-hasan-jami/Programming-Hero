e557064c01400417d6669d5193b7c4e49538f7d1
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-06 23:04:52 +0600

    added transaction-history functionality when money will be credited & debited

--- Project/mobile-banking-app/script/addMoney.js
+++ Project/mobile-banking-app/script/addMoney.js
@@ -46,4 +46,38 @@ document.getElementById("btn-addmoney").addEventListener("click", function (even
                 </div>`
         invalidDiv.appendChild(invalidMessage);
     }
+
+    // show the transaction history
+    let bankSelected = document.getElementById("allbank").value;
+    let accountNumber = document.getElementById("account-number").value;
+    let transactionType = "Credited";
+    let transactionAmount = convertedAmount;
+    let transactionDate = new Date().toLocaleString();
+
+    let transactionHistory = document.getElementById("transaction-history");
+    let transaction = document.createElement("div");
+    transaction.innerHTML =
+        `<div class="overflow-x-auto">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Transaction Type</th>
+                        <th>Bank</th>
+                        <th>Transaction Amount</th>
+                        <th>Account Number</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>${transactionType}</td>
+                        <td>${bankSelected}</td>
+                        <td>${transactionAmount}</td>
+                        <td>${accountNumber}</td>
+                        <td>${transactionDate}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>`;
+    transactionHistory.appendChild(transaction);
 })
