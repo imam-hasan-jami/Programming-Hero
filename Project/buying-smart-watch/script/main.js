f44482ae0f5bf4238845cf56e512a970ce5e9eeb
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-08 15:52:56 +0600

    added band-color changing functionality

---
+++ Project/buying-smart-watch/script/main.js
@@ -0,0 +1,15 @@
+const ringButtons = document.querySelectorAll(".ring-button");
+
+for (let i = 0; i < ringButtons.length; i++) {
+    const ringBtn = ringButtons[i];
+    ringBtn.addEventListener("click", function (event) {
+
+        for (let j = 0; j < ringButtons.length; j++) {
+            ringButtons[j].classList.remove("border-purple-600");
+            ringButtons[j].classList.add("border-gray-300");
+        }
+
+        event.target.classList.add("border-purple-600");
+        event.target.classList.remove("border-gray-300");
+    })
+}
