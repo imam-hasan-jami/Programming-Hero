5b062f45c8e81c7d314013f510136949ea337d54
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-13 00:41:40 +0600

    video api is called and all the data is visible in the webpage

--- Project/ph-tube-API/script/index.js
+++ Project/ph-tube-API/script/index.js
@@ -4,6 +4,12 @@ function loadCategories() {
         .then((data) => displayCategories(data.categories));
 }
 
+function loadVideos() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((response) => response.json())
+        .then((data) => displayVideos(data.videos));
+}
+
 function displayCategories(categories) {
     const categoryContainer = document.getElementById("category-container");
 
@@ -20,4 +26,57 @@ function displayCategories(categories) {
     }
 }
 
+function displayVideos(videos) {
+    const videoContainer = document.getElementById("video-container");
+
+    if (videos.length == 0) {
+        videoContainer.innerHTML = `
+        <div
+        class="py-20 col-span-full flex flex-col justify-center items-center text-center">
+            <img class="w-[120px]" src="./assets/Icon.png" alt="" />
+            <h2 class="text-2xl font-bold">
+                Oops!! Sorry, There is no content here
+            </h2>
+        </div>
+        `;
+        return;
+    }
+
+    for (let video of videos) {
+        const videoCard = document.createElement("div");
+
+        videoCard.innerHTML = `
+            <div class="card bg-base-100">
+                <figure class="relative">
+                    <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+                    <span
+                    class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2">3hrs 56 min ago</span>
+                </figure>
+            </div>
+
+            <div class="flex gap-3 px-0 py-5">
+                <div class="profile">
+                    <div class="avatar">
+                        <div
+                        class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                        <img src="${video.authors[0].profile_picture}"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="intro">
+                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                <p class="text-sm text-gray-400 flex gap-1">
+                    ${video.authors[0].profile_name}
+                    ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="" />` : ``}
+                </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+        `;
+
+        videoContainer.append(videoCard);
+    }
+}
+
 loadCategories();
+loadVideos();
