d19f82663373e5b4fde91fca0f2e2bcf6535fcea
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-18 16:06:51 +0600

    js local storage object, json.stringify, json.parse

--- Practice/JavaScript/LocalStorage/index.js
+++ Practice/JavaScript/LocalStorage/index.js
@@ -1,3 +1,4 @@
+/*
 localStorage.setItem("name", "ronaldo");
 localStorage.setItem("age", "40");
 localStorage.setItem("club", "real madrid");
@@ -6,3 +7,30 @@ localStorage.getItem("name");
 
 // localStorage.clear();
 localStorage.removeItem("club");
+*/
+
+/*
+const handleAddToStorage = () => {
+    const name = "cr7";
+    localStorage.setItem("name", name);
+}
+*/
+
+/*
+const handleAddToStorage = () => {
+    const person = {
+        name: "rudiger",
+        age: 32,
+        friends: ["asencio", "ramos"]
+    }
+
+    // localStorage.setItem("person", person);
+    // localStorage.setItem("person", JSON.stringify(person));
+    const convertedToString = JSON.stringify(person);
+    localStorage.setItem("person", convertedToString);
+    console.log(convertedToString);
+
+    const backToObject = JSON.parse(convertedToString);
+    console.log(backToObject);
+}
+*/
