d994060d98dd3df7134a34ece05ec1f5f37ba002
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-19 03:21:07 +0600

    PROPS in react

--- Practice/React/first-react-app/src/App.jsx
+++ Practice/React/first-react-app/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Student from './Student'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+      <>
+        <Student name="Sadi" age={23} isStudent={true} />
+        <Student name="Rahat" age={18} isStudent={true} />
+        <Student name="Kamal" age={45} isStudent={false} />
+      </>
+    )
 }
 
 export default App
