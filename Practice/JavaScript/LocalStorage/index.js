ae470d5332138dbe45c4bb45a1b41ee6db87af5f
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-18 16:58:09 +0600

    store data in local storage and get-show data from local storage

--- Practice/JavaScript/LocalStorage/index.js
+++ Practice/JavaScript/LocalStorage/index.js
@@ -34,3 +34,54 @@ const handleAddToStorage = () => {
     console.log(backToObject);
 }
 */
+
+const handleAddToCart = () => {
+    const productName = document.getElementById("product-name").value;
+    const productQuantity = document.getElementById("product-quantity").value;
+
+    // console.log(productName, productQuantity);
+    displayProduct(productName, productQuantity);
+    setValueToLocalStorage(productName, productQuantity);
+
+    document.getElementById("product-name").value = "";
+    document.getElementById("product-quantity").value = "";
+}
+
+const displayProduct = (productName, productQuantity) => {
+    const productContainer = document.getElementById("product-container");
+    const li = document.createElement("li");
+    li.innerText = `${productName} : ${productQuantity}`;
+    productContainer.append(li);
+}
+
+const getProductFromLocalStorage = () => {
+    let cart = {};
+    const getProduct = localStorage.getItem("cart");
+
+    if (getProduct) {
+        cart = JSON.parse(getProduct);
+    }
+    return cart;
+}
+
+const setValueToLocalStorage = (productName, productQuantity) => {
+    const cart = getProductFromLocalStorage();
+
+    cart[productName] = productQuantity;
+
+    const cartString = JSON.stringify(cart);
+
+    localStorage.setItem("cart", cartString);
+}
+
+const displayProductFromLocalStorage = () => {
+    const products = getProductFromLocalStorage();
+
+    for (let product in products) {
+        // console.log(product);
+        // console.log(products[product]);
+        displayProduct(product, products[product]);
+    }
+}
+
+displayProductFromLocalStorage();
