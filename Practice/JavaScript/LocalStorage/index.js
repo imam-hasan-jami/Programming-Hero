af6696d8fffe452002670cc8f10d869c89437df3
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-18 15:37:51 +0600

    js local storage basic concept

---
+++ Practice/JavaScript/LocalStorage/index.js
@@ -0,0 +1,8 @@
+localStorage.setItem("name", "ronaldo");
+localStorage.setItem("age", "40");
+localStorage.setItem("club", "real madrid");
+
+localStorage.getItem("name");
+
+// localStorage.clear();
+localStorage.removeItem("club");
