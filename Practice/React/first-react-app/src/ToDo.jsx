1f4aeaca3db30bc308fedbedebffcdfffad462b1
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-23 02:47:19 +0600

    more ways of conditional rendering

--- Practice/React/first-react-app/src/ToDo.jsx
+++ Practice/React/first-react-app/src/ToDo.jsx
@@ -4,11 +4,37 @@ function ToDo({task, time, isDone}) {
     //         <p>Done: {task}, Duration: {time}</p>
     //     </>
     // )
+
+    // **conditional rendering: 1 (if else)
     if (isDone) {
         return <p>Done: {task}, Duration: {time}</p>
+    } else {
+        return <p>To be done: {task}, Time need: {time}</p>
     }
 
-    return <p>To be done: {task}, Time need: {time}</p>
+    // **conditional rendering: 1 (if else) another version
+    // if (isDone) {
+    //     return null
+    // } else {
+    //     return <p>To be done: {task}, Time need: {time}</p>
+    // }
+
+    // **conditional rendering: 2 (if) no need of else, if the statement is false, it will return the false without the need of else statement.
+    // if (isDone) {
+    //     return <p>Done: {task}, Duration: {time}</p>
+    // }
+    // return <p>To be done: {task}, Time need: {time}</p>
+
+    // **conditional rendering: 3 (ternary operator)
+    // return isDone ? <p>Done: {task}, Duration: {time}</p> : <p>To be done: {task}, Time need: {time}</p>
+
+    // **conditional rendering: 4 (using and(&&) operator) will show the true statement
+    // return isDone && <p>Done: {task}, Duration: {time}</p>
+
+    // **conditional rendering: 5 (using or(||) operator) will show the false statement
+    // return isDone || <p>To be done: {task}, Time need: {time}</p>
+
+    
 }
 
 export default ToDo
