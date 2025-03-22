233414109326999003512d6fa2bc4960e58f47ab
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-03-22 23:36:02 +0600

    basic props

--- Practice/React/first-react-app/src/App.jsx
+++ Practice/React/first-react-app/src/App.jsx
@@ -5,14 +5,43 @@ function App() {
     return (
       <>
         <Student name="Sadi" age={23} isStudent={true} />
-        <Student name="Rahat" age={18} isStudent={true} />
-        <Student name="Kamal" age={45} isStudent={false} />
         {/* using default values provided in the parameter */}
         <Student />
         {/* uses only jamal for name and default parameter value for rest of the values */}
         <Student name="Jamal" />
+
+        {/* calling components declared in the same file */}
+        <Sport name="Cricket" format="T20" />
+        <Sport name="Football" format="90min" />
+        <Pet />
+        <Device name="iPhone" />
       </>
     )
+}
+
+function Sport(props) {
+  return(
+    <>
+      <ul>
+        <li>{props.name}, {props.format}</li>
+      </ul>
+    </>
+  )
+}
+
+function Pet() {
+  const petName = "Mimi";
+  return(
+    <p>I have a pet named {petName}</p>
+  )
+}
+
+function Device(props) {
+  return(
+    <>
+      <p>My primary device is {props.name}</p>
+    </>
+  )
 }
 
 export default App
