283cc72c49939758010b556ec9da3222e9ede2dc
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-15 12:43:53 +0600

    create charts using recharts

--- Practice/React/react-custom-design-app/src/App.jsx
+++ Practice/React/react-custom-design-app/src/App.jsx
@@ -2,6 +2,7 @@ import DaisyNavbar from "./Components/DaisyNavbar/DaisyNavbar";
 import Navbar from "./Components/Navbar/Navbar";
 import { Suspense } from "react";
 import PricingOptions from "./Components/PricingOptions/PricingOptions";
+import ResultsChart from "./Components/ResultsChart/ResultsChart";
 
 const pricingPromise = fetch("pricingData.json").then(res => res.json());
 
@@ -13,6 +14,8 @@ function App() {
             <Suspense fallback={<span className="loading loading-spinner loading-lg"></span>}>
                 <PricingOptions pricingPromise={pricingPromise} />
             </Suspense>
+
+            <ResultsChart />
         </>
     );
 }
