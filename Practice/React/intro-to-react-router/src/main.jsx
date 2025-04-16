bcc441bf777fcc33d5cb753964a08a63060535eb
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-16 22:22:13 +0600

    created some route using react-router

--- Practice/React/intro-to-react-router/src/main.jsx
+++ Practice/React/intro-to-react-router/src/main.jsx
@@ -2,16 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from './Components/Root/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Mobiles from './Components/Mobiles/Mobiles.jsx';
+import Laptops from './Components/Laptops/Laptops.jsx'
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello from React Router!</div>,
+        Component: Root,
+        children: [
+          { index: true, Component: Home },
+          { path: "mobiles", Component: Mobiles },
+          { path: "laptops", Component: Laptops },
+        ]
     },
+    {
+        path: "about",
+        element: <div>This is About me section</div>
+    },
+    {
+        path: "blogs",
+        element: <div>All my blogs are here</div>
+    },
+    {
+        path: "app",
+        // use element or Component like the below example
+        // element: <App />
+        Component: App
+    }
 ]);
 
 createRoot(document.getElementById("root")).render(
