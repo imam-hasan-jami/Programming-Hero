0115936455a78309a9070dc0cb6cfddec6a3a87a
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-17 12:55:50 +0600

    usage of pendingUI - useNavigation hook in Root.jsx for loader screen

--- Practice/React/intro-to-react-router/src/Components/Root/Root.jsx
+++ Practice/React/intro-to-react-router/src/Components/Root/Root.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer'
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import SideBar from '../SideBar/SideBar';
 import './Root.css'
 
 const Root = () => {
+
+    const navigation = useNavigation();
+    const isNavigating = Boolean(navigation.location);
+
     return (
         <div>
             <Header />
             <div className='flex items-center gap-10'>
                 <SideBar />
+                {isNavigating && <span>Loading....</span>}
                 <Outlet />
             </div>
             <Footer />
