ac1ede43e1707198ad99a8be7a085c2bf94c0e81
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-17 14:33:52 +0600

    usage of Navigate component to go to specific location

--- Practice/React/intro-to-react-router/src/Components/User/User.jsx
+++ Practice/React/intro-to-react-router/src/Components/User/User.jsx
@@ -1,16 +1,24 @@
 import React, { Suspense, useState } from 'react';
-import { Link } from 'react-router';
+import { Link, Navigate } from 'react-router';
 import UserDetails2 from '../UserDetails2/UserDetails2';
 
 const User = ({ user }) => {
     const [showInfo, setShowInfo] = useState(false);
 
     const {id, name, email, phone} = user;
-
+    
     const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json());
+    
+    const [visitHome, setVisitHome] = useState(false);
+    if(visitHome) {
+        return <Navigate to="/" />
+    }
 
     return (
         <div>
+
+            <button onClick={() => setVisitHome(true)} className='mt-5'>Go to Home</button>
+
             <div className='bg-orange-100 text-black border-2 border-amber-300 p-5 m-5 rounded-lg'>
                 <h3>Name: {name}</h3>
                 <p>Email: {email}</p>
