2ddbf79233ccc94aa07f7be9ebaafb5747bc7f11
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-17 13:08:37 +0600

    usage of useParams inside a component

--- Practice/React/intro-to-react-router/src/Components/UserDetails/UserDetails.jsx
+++ Practice/React/intro-to-react-router/src/Components/UserDetails/UserDetails.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+// eslint-disable-next-line no-unused-vars
+import { useLoaderData, useParams } from 'react-router';
 
 const UserDetails = () => {
     const user = useLoaderData();
 
-    const {name, username, email, phone, website} = user;
+    const {id, name, username, email, phone, website} = user;
+
+    // const params = useParams();
+    // console.log('params id',params.userId);
+
+    // const { userId } = useParams();
+    // console.log(userId);
 
     return (
         <div>
@@ -12,6 +19,7 @@ const UserDetails = () => {
 
             <div className='bg-orange-100 text-black border-2 border-amber-300 p-5 m-5 rounded-lg'>
                 <h3 className='font-bold'>Name: {name}</h3>
+                <p>userId: {id}</p>
                 <p>Username: {username}</p>
                 <p>Email: {email}</p>
                 <p>Phone: {phone}</p>
