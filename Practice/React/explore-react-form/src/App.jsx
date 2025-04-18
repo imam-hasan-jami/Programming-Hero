d1b57c3bbafa18bb2afce9cb525f50ee45fed59b
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-18 12:46:21 +0600

    project setup + 2 different way to get form data

---
+++ Practice/React/explore-react-form/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import FormAction from './Components/FormAction/FormAction'
+import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>Explore React Form</h1>
+
+      <SimpleForm />
+
+      <FormAction />
+    </>
+  )
+}
+
+export default App
