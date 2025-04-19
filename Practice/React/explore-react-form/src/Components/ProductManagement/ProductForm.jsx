ff8696a5f3bebad194d89cb5acc2443316bbd4e0
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-19 10:35:07 +0600

    display form data in a table and form error handling

--- Practice/React/explore-react-form/src/Components/ProductManagement/ProductForm.jsx
+++ Practice/React/explore-react-form/src/Components/ProductManagement/ProductForm.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({handleAddProduct}) => {
+    const [error, setError] = useState("");
+
     const handleProductSubmit = (e) => {
         e.preventDefault();
 
@@ -10,13 +12,32 @@ const ProductForm = () => {
 
         // console.log(name, price, quantity);
 
+        if (name.length === 0) {
+            setError("Please enter the product name");
+            return;
+        } else if (price.length === 0) {
+            setError("Please enter the product price");
+            return;
+        } else if (price < 0) {
+            setError("Price can not be negative");
+            return;
+        } else if (quantity.length === 0) {
+            setError("Please enter the product quantity ");
+            return;
+        } else {
+            setError("");
+        }
+
         const newProduct = {
             name,
             price,
             quantity
         }
 
-        console.log(newProduct);
+        // console.log(newProduct);
+        if (!error) {
+            handleAddProduct(newProduct);
+        }
     }
 
     return (
@@ -31,6 +52,7 @@ const ProductForm = () => {
                 <br />
                 <input type="submit" value="Submit" />
             </form>
+            <p style={{color: "red"}}>{error}</p>
         </div>
     );
 };
