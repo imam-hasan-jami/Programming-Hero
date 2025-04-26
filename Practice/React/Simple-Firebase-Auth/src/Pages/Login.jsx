2a4f15bd0aa19feb59fcdf789562cfd28e3f739e
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-27 01:32:46 +0600

    display signed in user info & sign out

--- Practice/React/Simple-Firebase-Auth/src/Pages/Login.jsx
+++ Practice/React/Simple-Firebase-Auth/src/Pages/Login.jsx
@@ -1,15 +1,18 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../Firebase/firebase.init';
 
 const Login = () => {
+    const [user, setUser] = useState(null);
     const provider = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
         console.log("Google Sign In");
+        
 
         signInWithPopup(auth, provider)
             .then(result => {
+                setUser(result.user);
                 console.log(result);
             })
             .catch(error => {
@@ -17,12 +20,44 @@ const Login = () => {
             })
     }
 
+    const handleGoogleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("sign out completed");
+                setUser(null);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
         <div>
-            <h1 className='text-5xl text-center mt-20'>Please Login</h1>
-            <div className='flex justify-center items-center mt-10'>
-                <button onClick={handleGoogleSignIn} className='bg-blue-500 text-white px-5 py-2 rounded-lg'>Sign in with Google</button>
+            <h1 className="text-5xl text-center mt-20">Please Login</h1>
+            <div className="flex gap-5 justify-center items-center mt-10">
+                {user ? (
+                    <button
+                        onClick={handleGoogleSignOut}
+                        className="bg-blue-500 text-white px-5 py-2 rounded-lg"
+                    >
+                        Sign Out
+                    </button>
+                ) : (
+                    <button
+                        onClick={handleGoogleSignIn}
+                        className="bg-blue-500 text-white px-5 py-2 rounded-lg"
+                    >
+                        Sign in with Google
+                    </button>
+                )}
             </div>
+            {user && (
+                <div className="flex flex-col justify-center items-center gap-5 mt-10">
+                    <h3>{user.displayName}</h3>
+                    <p>{user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            )}
         </div>
     );
 };
