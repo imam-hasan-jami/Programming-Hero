260697b77e6a7490d740b42ad2421908a0e79675
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-27 13:30:22 +0600

    project & router setup

---
+++ Practice/React/explore-email-password-auth/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from './layout/Root.jsx';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+            {
+              path: "login",
+              Component: Login,
+            }
+        ],
+    },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
---
+++ Practice/React/explore-email-password-auth/src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1 className='text-center text-5xl mt-10 font-extrabold'>This is Login</h1>
+        </div>
+    );
+};
+
+export default Login;
