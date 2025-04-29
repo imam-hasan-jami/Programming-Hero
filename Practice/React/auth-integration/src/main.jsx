176e06caea2790d875963181110db21d7c0b45a0
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-29 11:48:42 +0600

    created pages, layout, navbar & setup react-router

--- Practice/React/auth-integration/src/main.jsx
+++ Practice/React/auth-integration/src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from './pages/Home';
+import Root from './layouts/Root';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: Root,
+        children: [
+          {
+            index: true,
+            Component: Home,
+          },
+          {
+            path: "login",
+            Component: Login,
+          },
+          {
+            path: "signup",
+            Component: Signup,
+          }
+        ]
+    },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
