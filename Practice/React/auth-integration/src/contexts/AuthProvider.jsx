506b7498c6ea85f4029a93db6d6e3d46431865e9
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-04-29 23:55:38 +0600

    google sign in

--- Practice/React/auth-integration/src/contexts/AuthProvider.jsx
+++ Practice/React/auth-integration/src/contexts/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../firebase.init";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
@@ -15,6 +17,11 @@ const AuthProvider = ({ children }) => {
     const logInUser = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
     }
 
     const signOutUser = () => {
@@ -55,6 +62,7 @@ const AuthProvider = ({ children }) => {
         loading,
         createUser,
         logInUser,
+        googleSignIn,
         signOutUser,
     }
 
--- Practice/React/auth-integration/src/pages/Login.jsx
+++ Practice/React/auth-integration/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation, useNavigate } from 'react-router';
 import { AuthContext } from '../contexts/AuthContext';
 
 const Login = () => {
-    const { logInUser } = use(AuthContext);
+    const { logInUser, googleSignIn } = use(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     console.log(location );
@@ -23,8 +23,19 @@ const Login = () => {
             })
     }
 
+    const handleSignInWithGoogle = () => {
+        googleSignIn()
+            .then(result => {
+                console.log(result.user);
+                navigate(location?.state || "/");
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
-        <div className='flex flex-col items-center justify-center mt-20'>
+        <div className="flex flex-col items-center justify-center mt-20">
             <div className="text-center lg:text-left">
                 <h1 className="text-5xl font-bold mb-5">Login Now!</h1>
             </div>
@@ -36,14 +47,14 @@ const Login = () => {
                             type="email"
                             className="input"
                             placeholder="Email"
-                            name='email'
+                            name="email"
                         />
                         <label className="label">Password</label>
                         <input
                             type="password"
                             className="input"
                             placeholder="Password"
-                            name='password'
+                            name="password"
                         />
                         <div>
                             <a className="link link-hover">Forgot password?</a>
@@ -52,8 +63,44 @@ const Login = () => {
                     </form>
                 </div>
             </div>
+            <button onClick={handleSignInWithGoogle} className="btn bg-white text-black border-[#e5e5e5] mt-5">
+                <svg
+                    aria-label="Google logo"
+                    width="16"
+                    height="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                >
+                    <g>
+                        <path d="m0 0H512V512H0" fill="#fff"></path>
+                        <path
+                            fill="#34a853"
+                            d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                        ></path>
+                        <path
+                            fill="#4285f4"
+                            d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                        ></path>
+                        <path
+                            fill="#fbbc02"
+                            d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                        ></path>
+                        <path
+                            fill="#ea4335"
+                            d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                        ></path>
+                    </g>
+                </svg>
+                Login with Google
+            </button>
             <div className="justify-center mt-6">
-                <p>Don't have an account? <Link className='text-blue-500 underline' to="/signup">Sign Up</Link> now!</p>
+                <p>
+                    Don't have an account?{" "}
+                    <Link className="text-blue-500 underline" to="/signup">
+                        Sign Up
+                    </Link>{" "}
+                    now!
+                </p>
             </div>
         </div>
     );
