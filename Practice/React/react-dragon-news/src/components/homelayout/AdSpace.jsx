0f8e07932421f18187e65024204d472b4ba20d57
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-03 21:55:12 +0600

    design right navbar (aside) components

---
+++ Practice/React/react-dragon-news/src/components/homelayout/AdSpace.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import adBg from "../../assets/bg.png"
+
+const AdSpace = () => {
+    return (
+        <div>
+            <img src={adBg} className='w-full' alt="" />
+        </div>
+    );
+};
+
+export default AdSpace;
--- Practice/React/react-dragon-news/src/components/homelayout/RightAside.jsx
+++ Practice/React/react-dragon-news/src/components/homelayout/RightAside.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import SocialLogin from './SocialLogin';
+import FindUs from './FindUs';
+import QZone from './Qzone';
+import AdSpace from './AdSpace';
 
 const RightAside = () => {
     return (
-        <div>
+        <div className='space-y-8'>
             <SocialLogin />
+            <FindUs />
+            <QZone />
+            <AdSpace />
         </div>
     );
 };
