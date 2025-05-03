bf5a0d14dbc5c5808510418e3a6b87ac94825b0b
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-03 21:43:20 +0600

    fetch & display news on NewsCard jsx

--- Practice/React/react-dragon-news/src/pages/CategoryNews.jsx
+++ Practice/React/react-dragon-news/src/pages/CategoryNews.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
+import NewsCard from '../components/NewsCard';
 
 const CategoryNews = () => {
     const {id} = useParams();
+    const data = useLoaderData();
+    const [categoryNews, setCategoryNews] = useState([]);
+    
+    useEffect(() => {
+      if (id == "0") {
+        setCategoryNews(data);
+        return;
+      } else if (id == "1") {
+        const filteredData = data.filter(
+          (news) => news.others.is_today_pick == true
+        );
+        setCategoryNews(filteredData);
+        return;
+      }
+      const filteredData = data.filter((news) => news.category_id == id);
+      setCategoryNews(filteredData);
+    }, [id, data]);
 
     return (
         <div>
-            CategoryNews - {id}
+            <h1 className='font-bold mb-5'>Total <span className='text-secondary'>{categoryNews.length}</span> news found</h1>
+
+            <div className='grid grid-cols-1 gap-5'>
+                {
+                    categoryNews.map((news) => (<NewsCard key={news.id} news={news}/>))
+                }
+            </div>
         </div>
     );
 };
