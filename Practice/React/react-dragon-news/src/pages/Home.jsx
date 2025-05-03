f2a833824956aea0265dde75db9914fb1e96e4ab
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-03 22:24:04 +0600

    use of Navigate hook

--- Practice/React/react-dragon-news/src/pages/Home.jsx
+++ Practice/React/react-dragon-news/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
         <div>
-            Home Page
+            {/* Home Page */}
+            <Navigate to="/category/1" />
         </div>
     );
 };
