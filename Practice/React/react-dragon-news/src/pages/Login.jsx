f3e93c97f5cdd5a57946297c6c2d2505550c0dc6
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-06 00:47:51 +0600

    created login page

--- Practice/React/react-dragon-news/src/pages/Login.jsx
+++ Practice/React/react-dragon-news/src/pages/Login.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+        <h2 className="font-semibold text-2xl text-center">
+          Login your account
+        </h2>
+        <form className="card-body">
+          <fieldset className="fieldset">
+            {/* email  */}
+            <label className="label">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              required
+            />
+            {/* passowrd  */}
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+
+            {/* {error && <p className="text-red-400 text-xs">{error}</p>} */}
+
+            <button type="submit" className="btn btn-neutral mt-4">
+              Login
+            </button>
+            <p className="font-semibold text-center pt-5">
+              Dont’t Have An Account ?{" "}
+              <Link className="text-secondary" to="/auth/register">
+                Register
+              </Link>
+            </p>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
