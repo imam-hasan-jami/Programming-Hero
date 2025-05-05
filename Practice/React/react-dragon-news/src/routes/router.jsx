89f0e8d73083170af5c3fefb3518170888478066
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-05 23:44:46 +0600

    defines auth routes

--- Practice/React/react-dragon-news/src/routes/router.jsx
+++ Practice/React/react-dragon-news/src/routes/router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layouts/HomeLayout";
 import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +22,20 @@ const router = createBrowserRouter([
         },
     ],
   },
+  {
+    path: "/auth",
+    Component: AuthLayout,
+    children: [
+        {
+            path: "/auth/login",
+            Component: Login,
+        },
+        {
+            path: "/auth/register",
+            Component: Register,
+        }
+    ]
+  }
 ]);
 
 export default router;
