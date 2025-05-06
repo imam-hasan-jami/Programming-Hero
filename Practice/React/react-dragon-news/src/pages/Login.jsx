b4cfbbf6ade47b176145fadedd8ddbb787df44a5
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-06 18:22:00 +0600

    deployed to firebase

--- Practice/React/react-dragon-news/src/pages/Login.jsx
+++ Practice/React/react-dragon-news/src/pages/Login.jsx
@@ -15,12 +15,12 @@ const Login = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
 
-        console.log(email, password);
+        // console.log(email, password);
 
         loginUser(email, password)
           .then((result) => {
             const user = result.user;
-            console.log(user);
+            // console.log(user);
             navigate(`${location.state ? location.state : "/"}`);
           })
           .catch((error) => {
