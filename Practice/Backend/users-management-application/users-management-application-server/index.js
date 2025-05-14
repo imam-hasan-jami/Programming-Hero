7dc600b5c7f0dc3c4a87765ba0fd513692c8cb0b
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-15 04:14:55 +0600

    Create a client-side post and Insert single-user data

--- Practice/Backend/users-management-application/users-management-application-server/index.js
+++ Practice/Backend/users-management-application/users-management-application-server/index.js
@@ -1,10 +1,49 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+//user: simpleDBUser
+//password: PPA1BOcgLuUzXVBL
+
+const uri = "mongodb+srv://simpleDBUser:PPA1BOcgLuUzXVBL@cluster0.lqn2pwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+}); 
+
+async function run() {
+    try {
+    //   await client.connect();
+
+        const database = client.db('usersDB');
+        const usersCollection = database.collection('users');
+
+        app.post("/users", async(req, res) => {
+          console.log('data in the server', req.body);
+          const newUser = req.body;
+          const result = await usersCollection.insertOne(newUser);
+          res.send(result);
+        });
+
+      await client.db("admin").command({ ping: 1 });
+      console.log(
+        "Pinged your deployment. You successfully connected to MongoDB!"
+      );
+    } finally {
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Users Management Server is Running');
@@ -20,14 +59,14 @@ app.get('/users', (req, res) => {
     res.send(users);
 })
 
-app.post('/users', (req, res) => {
-    const newUser = req.body;
-    newUser.id = users.length + 1;
+// app.post('/users', (req, res) => {
+//     const newUser = req.body;
+//     newUser.id = users.length + 1;
 
-    users.push(newUser);
+//     users.push(newUser);
 
-    res.send(newUser);
-})
+//     res.send(newUser);
+// })
 
 app.listen(port, () => {
     console.log(`Server is running on port, ${port}`);
