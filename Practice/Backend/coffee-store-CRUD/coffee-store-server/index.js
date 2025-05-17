b4723da0fa6b06f2ad3e6ba7695a063cd8b8f687
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-17 15:18:53 +0600

    project setup + post method
    project setup, installed dependencies, mongoDB uri, used .env for hiding data

---
+++ Practice/Backend/coffee-store-CRUD/coffee-store-server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lqn2pwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const coffeesCollection = client.db('coffeeStoreDB').collection('coffees');
+
+    app.post('/coffees', async(req, res) => {
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        const result = await coffeesCollection.insertOne(newCoffee);
+        res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello from Coffee Store Server!')
+})
+
+app.listen(port, () => {
+    console.log('Coffe Store Server is running on port', port);
+})
