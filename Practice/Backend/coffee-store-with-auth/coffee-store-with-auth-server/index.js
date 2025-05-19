3cc2d9da680b17f7d1367df7a2ad07d48cdec54a
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-05-19 17:09:03 +0600

    get method for users

--- Practice/Backend/coffee-store-with-auth/coffee-store-with-auth-server/index.js
+++ Practice/Backend/coffee-store-with-auth/coffee-store-with-auth-server/index.js
@@ -78,6 +78,11 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res) => {
         const userProfile = req.body;
         console.log(userProfile);
