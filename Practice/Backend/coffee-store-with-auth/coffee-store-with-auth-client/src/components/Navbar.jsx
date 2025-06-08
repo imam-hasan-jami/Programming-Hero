d9b7a219c244d02a438e41f9f1fb2bf7ced22f62
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-06-08 19:27:52 +0600

    generate token by calling api

--- Practice/Backend/coffee-store-with-auth/coffee-store-with-auth-client/src/components/Navbar.jsx
+++ Practice/Backend/coffee-store-with-auth/coffee-store-with-auth-client/src/components/Navbar.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { use } from "react";
+import { Link } from "react-router";
+import { AuthContext } from "../contexts/AuthContext";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
-    return (
-        <div className='navbar flex justify-center items-center gap-4 bg-base-300'>
-            <Link to='/'>Home</Link>
-            <Link to='/add-coffee'>Add Coffee</Link>
-            <Link to='/users'>Users</Link>
-            <Link to='/registration'>Registration</Link>
-            <Link to='/login'>Login</Link>
-        </div>
-    );
+  const { user, logOut } = use(AuthContext);
+
+  const handleLogout = () => {
+    logOut()
+      .then(() => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: `Logout successfully.`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
+  return (
+    <div className="navbar flex justify-center items-center gap-4 bg-base-300">
+      <Link to="/">Home</Link>
+      <Link to="/add-coffee">Add Coffee</Link>
+      <Link to="/users">Users</Link>
+      <Link to="/registration">Registration</Link>
+      {/* <Link to='/login'>Login</Link> */}
+      {user ? (
+        <button
+          onClick={handleLogout}
+          className="btn bg-red-600 text-white px-5"
+        >
+          Logout
+        </button>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
--- Practice/Backend/coffee-store-with-auth/coffee-store-with-auth-client/src/contexts/AuthProvider.jsx
+++ Practice/Backend/coffee-store-with-auth/coffee-store-with-auth-client/src/contexts/AuthProvider.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init';
+import axios from 'axios';
 
 const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
@@ -13,9 +17,41 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+      return signOut(auth);
+    };
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+
+        if (currentUser?.email) {
+          const userData = { email: currentUser.email };
+          axios
+            .post("http://localhost:3000/jwt", userData)
+            .then((res) => {
+              console.log("token after login jwt", res.data);
+              const token = res.data.token;
+              localStorage.setItem("access-token", token);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+
+        // console.log("User logged in:", currentUser);
+      });
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+
     const userInfo = {
+        user,
         createUser,
         loginUser,
+        logOut,
     }
 
     return (
