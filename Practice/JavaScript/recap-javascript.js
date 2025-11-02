c17ab70644f4d321985871aa769062b3306c57f9
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-11-02 22:38:38 +0600

    js break statement

--- Practice/JavaScript/recap-javascript.js
+++ Practice/JavaScript/recap-javascript.js
@@ -11,8 +11,53 @@
 // }
 
 // do while loop
-let i=0;
-do {
+// let i=0;
+// do {
+//     console.log(`the number is ${i}`);
+//     i++;
+// } while(i<5);
+
+// break in loop
+for(let i=0; i<5; i++) {
+    if(i==3) {
+        break;
+    }
     console.log(`the number is ${i}`);
-    i++;
-} while(i<5);
+}
+
+// break in switch
+// new Date().getDay() : you get the today's day. Sunday starts from 0
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+console.log(day);
+
+// labeled break
+loop1: for(let i=1; i<5; i++) {
+    loop2: for(let j=1; j<5; j++) {
+        if(j==3) {
+            break loop1;
+        }
+        console.log(`the number is ${j}`);
+    }
+}
