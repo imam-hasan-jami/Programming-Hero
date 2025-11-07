a76bdcb456acbc6d1d7dc879341925bffa472ed7
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-11-08 03:14:14 +0600

    Linking & Navigation
    use of <Link></Link> component for normal navigation; use of useRouter hook from 'next/link' for logical navigation

--- Practice/Next.js/nextjs-app/src/app/about/page.jsx
+++ Practice/Next.js/nextjs-app/src/app/about/page.jsx
@@ -1,9 +1,31 @@
+"use client"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function AboutPage() {
+
+  const router = useRouter()  // imported from 'next/link' || this is important as there are two 'useRouter' hook
+
+  const isLoggedIn = true;
+
+  const handleNavigation = () => {
+    if(isLoggedIn) {
+      router.push("/about/address");
+    } else {
+      router.push("/");
+    }
+  }
+
   return (
     <div>
+      <Link href="/about/address">
+        <li className='text-lg'>Address</li>
+      </Link>
+
       <h1>About Page!</h1>
+
+      <button className='text-sm p-3 m-5 text-black bg-amber-300 rounded-md cursor-pointer' type='button' onClick={handleNavigation}>Address Page</button>
     </div>
   )
 }
--- Practice/Next.js/nextjs-app/src/app/components/Navbar.jsx
+++ Practice/Next.js/nextjs-app/src/app/components/Navbar.jsx
@@ -1,30 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
     <div>
-      <nav className="flex justify-center items-center p-4 bg-gray-100 shadow-md">
+      <nav className="flex justify-center items-center p-4 bg-gray-600 shadow-md">
         <ul className="flex justify-between items-center w-3/12">
-          <li>
-            <a href="/" className="text-blue-500 hover:underline">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/services" className="text-blue-500 hover:underline">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="/about" className="text-blue-500 hover:underline">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/blog" className="text-blue-500 hover:underline">
-              Blog
-            </a>
-          </li>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/services">
+            <li>Services</li>
+          </Link>
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/blog">
+            <li>Blog</li>
+          </Link>
         </ul>
       </nav>
     </div>
