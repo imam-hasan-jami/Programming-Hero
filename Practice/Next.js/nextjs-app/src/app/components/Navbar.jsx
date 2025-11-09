29c1b38ff4a714ea1e958ae4284a243877668e9c
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-11-09 14:30:49 +0600

    Conditional layout

--- Practice/Next.js/nextjs-app/src/app/components/Navbar.jsx
+++ Practice/Next.js/nextjs-app/src/app/components/Navbar.jsx
@@ -1,25 +1,33 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function Navbar() {
-  return (
-    <div>
-      <nav className="flex justify-center items-center p-4 bg-gray-600 shadow-md">
-        <ul className="flex justify-between items-center w-3/12">
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/services">
-            <li>Services</li>
-          </Link>
-          <Link href="/about">
-            <li>About</li>
-          </Link>
-          <Link href="/blog">
-            <li>Blog</li>
-          </Link>
-        </ul>
-      </nav>
-    </div>
-  );
+  const pathname = usePathname();
+  // console.log(pathname, pathname.includes("dashboard"))
+  if (!pathname.includes("dashboard")) {
+    return (
+      <div>
+        <nav className="flex justify-center items-center p-4 bg-gray-600 shadow-md">
+          <ul className="flex justify-between items-center w-3/12">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/services">
+              <li>Services</li>
+            </Link>
+            <Link href="/about">
+              <li>About</li>
+            </Link>
+            <Link href="/blog">
+              <li>Blog</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
