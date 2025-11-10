70282e4abddaf93f06b5386ab64a3b737ff999d0
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-11-11 00:20:46 +0600

    Loading spinner

---
+++ Practice/Next.js/nextjs-app/src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingPage() {
+  return (
+    <div className="flex justify-center items-center">
+      <p className="text-xl">Loading...</p>
+    </div>
+  );
+}
